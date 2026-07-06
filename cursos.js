79284ad3f6bc7f2648c82f3bebf8c1c2850f5857
angelfarias867-wq <angelfarias867@gmail.com>
2026-07-06 15:11:08 -0400

    first commit

---
+++ cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+    'programacion': [
+        {
+            id:1,
+            titulo: 'Aprende Python',
+            lenguaje: 'python',
+            vistas: 15000,
+            nivel: 'basico'
+        },
+        {
+            id:2,
+            titulo: 'Aprende Python',
+            lenguaje: 'python',
+            vistas: 15000,
+            nivel: 'intermedio'
+        },
+        {
+            id:3,
+            titulo: 'Aprende JavaScript',
+            lenguaje: 'JavaScript',
+            vistas: 15000,
+            nivel: 'basico'
+        }
+    ],
+    'matematicas': [
+        {
+        id:1,
+            titulo: 'Aprende Calculo',
+            tema: 'Calculo',
+            vistas: 12400,
+            nivel: 'basico'
+        },
+        {
+            id:2,
+            titulo: 'Aprende Algebra',
+            tema: 'Algebra',
+            vistas: 13500,
+            nivel: 'intermedio'
+        }
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
