8c7018ab3c76665c7ef43885f49fb3868b64d5dd
angelfarias867-wq <angelfarias867@gmail.com>
2026-07-13 12:39:16 -0400

    Routers

--- routers/programacion.js
+++ routers/programacion.js
@@ -1,18 +1,33 @@
 const require = require('express');
-const {programacion} = require('../datos/cursos').InfoCursos;
+const {programacion} = require('../datos/cursos.js').InfoCursos;
 const routerProgramacion = express.Router();
 
 routerProgramacion.get('/', (req, res) => {
-    res.send(JSON.stringify(programacion));
+    res.send(JSON.stringify(InfoCursos.programacion));
 });
-
-router
 
 routerProgramacion.get('/:lenguaje', (req, res) => {
     const lenguaje = req.params.lenguaje;
-    const resultado = programacion.filter(curso => curso.lenguaje === lenguaje);
+    const resultado = InfoCursos.programacion.filter(curso => curso.lenguaje === lenguaje);
     if (resultado.length === 0) {
         return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
     }
+    
+    if (req.query.ordenar === 'vistas') {
+        return res.send(JSON.stringify(resultado.sort((a, b) => b.vistas - a.vistas)));
+    }
+
     res.send(JSON.stringify(resultado));
 });
+
+routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const nivel = req.params.nivel;
+    const resultado = InfoCursos.programacion.filter(curso => curso.lenguaje === lenguaje);
+    if (resultado.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+    }
+    res.send(JSON.stringify(resultado));
+});
+
+module.exports = routerProgramacion
